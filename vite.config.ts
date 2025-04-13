c34e453be4e1a9a7d49910ee33e0085ceb5fa984
Muhammed ahmed <muhameedahmed4472@gmail.com>
2025-04-14 00:43:54 +0200

    Deploy to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/MY-Portfolio/', 
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
