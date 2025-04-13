8765280b8ef0a0fb60dc9e323416c10d0cc86cf3
Muhammed ahmed <muhameedahmed4472@gmail.com>
2025-04-14 00:50:36 +0200

    Deploy to GitHub Pages

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -9,7 +9,7 @@ import Contact from '../pages/Contact';
 function AppRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/MY-Portfolio" element={<Home />} />
       <Route path="/about" element={<About />} />
       <Route path="/services" element={<Services />} />
       <Route path="/portfolio" element={<Portfolio />} />
