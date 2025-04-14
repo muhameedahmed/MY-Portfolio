72762cab78748adcb75bc7815dba930ebfb65e18
Muhammed ahmed <muhameedahmed4472@gmail.com>
2025-04-14 02:24:19 +0200

    w

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -8,7 +8,7 @@ interface LayoutProps {
 
 function Layout({ children }: LayoutProps) {
   return (
-    <div className="min-h-screen bg-white dark:bg-black transition-colors duration-200">
+    <div className=" bg-white dark:bg-black transition-colors duration-200">
       <Navbar />
       <main className="pt-16">{children}</main>
       <Footer />
